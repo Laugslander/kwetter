38c35af1392531eedb43ce88d5a25f157dbb49c2
Robin Laugs <robinlaugs@live.nl>
2018-03-10 14:45:05 +0100

    Added default test to custom exceptions

--- src/main/java/nl/robinlaugs/kwetter/exception/DuplicateUsernameException.java
+++ src/main/java/nl/robinlaugs/kwetter/exception/DuplicateUsernameException.java
@@ -4,4 +4,9 @@ package nl.robinlaugs.kwetter.exception;
  * @author Robin Laugs
  */
 public class DuplicateUsernameException extends Exception {
+
+    public DuplicateUsernameException() {
+        super("Username already exists");
+    }
+
 }
--- src/main/java/nl/robinlaugs/kwetter/exception/InvalidCredentialsException.java
+++ src/main/java/nl/robinlaugs/kwetter/exception/InvalidCredentialsException.java
@@ -4,4 +4,9 @@ package nl.robinlaugs.kwetter.exception;
  * @author Robin Laugs
  */
 public class InvalidCredentialsException extends Exception {
+
+    public InvalidCredentialsException() {
+        super("Username or password is not correct");
+    }
+
 }
